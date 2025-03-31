83f2d3db356b829ef1fa01c974faac71ca41bc96
josephruiz18101 <joseph123920@gmail.com>
2025-03-30 21:24:13 -0500

    Delete server directory

--- server/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { someController } = require('./controllers/chatController');
-
-router.post('/', someController);  // if not used, comment out
-
-module.exports = router;
